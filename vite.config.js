83e648ee983cf60aa3124f7b9162759779e428ef
ArellaKoo <arella.koo@bigtinygroup.com>
2025-01-12 19:15:08 +0800

    Test cicd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/My_Santa_Rush/', // Update this with your repository name
-  // other configurations...
+  // other configurations....
   plugins: [react()],
 })
